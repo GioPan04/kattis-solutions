12deedfaaddfc8dbf798db01d2a89b0d94d3f7c2
Gioele Pannetto <gioele@pannetto.com>
2021-10-09 15:18:08 +0200

    Added Carrots solution

---
+++ Carrots/src/it/gioelepannetto/carrots/Main.java
@@ -0,0 +1,12 @@
+package it.gioelepannetto.carrots;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    final Scanner input = new Scanner(System.in);
+	    input.nextInt();
+	    System.out.println(input.nextInt());
+    }
+}
